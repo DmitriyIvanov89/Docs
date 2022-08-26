6eb83a55782eb863f4cbd3f73af2ef99089c1c4d
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-08-26 11:12:30 +0300

    mod

--- 23962/20220818_23962.sql
+++ 23962/20220818_23962.sql
@@ -1,10 +1,10 @@
 INSERT INTO SP.EXPR
 (EXPR_ID, EXPR_NAME, EXPR_TYPE_CODE, EXPR_SCOPE_CODE, EXPR_TEXT, EXPR_CODE)
-VALUES(-55, 'TestБазовая сумма минус процент филиала от базовой суммы, если по ДТ валютная касса', 'groovy', 'OPER_LIM', 'return doc.cashDecrease & doc.creditSumma.foreignCurrency ? baseSum.subtract(baseSum.multiply(service.getParamService().getParamValueGlobalNumber("LIMITATION_SALE_PERCENT").divide(100))) : null', 'BASE_SUM_MINUS_LSP');
+VALUES(-11, 'Продажа_Базовая сумма минус процент филиала от базовой суммы, если по ДТ валютная касса', 'groovy', 'OPER_LIM', 'return doc.cashDecrease & doc.creditSumma.foreignCurrency ? baseSum.subtract(baseSum.multiply(service.getParamService().getParamValueGlobalNumber("LIMITATION_SALE_PERCENT").divide(100))) : null', 'BASE_SUM_MINUS_LSP');
 
 INSERT INTO SP.EXPR
 (EXPR_ID, EXPR_NAME, EXPR_TYPE_CODE, EXPR_SCOPE_CODE, EXPR_TEXT, EXPR_CODE)
-VALUES(-66, 'TestПроцент филиала от базовой суммы, если по ДТ валютная касса', 'groovy', 'OPER_LIM', 'return doc.cashDecrease & doc.creditSumma.foreignCurrency ? baseSum.multiply(service.getParamService().getParamValueGlobalNumber("LIMITATION_SALE_PERCENT").divide(100)) : null', 'BASE_SUM_LSP');
+VALUES(-12, 'Продажа_Процент филиала от базовой суммы, если по ДТ валютная касса', 'groovy', 'OPER_LIM', 'return doc.cashDecrease & doc.creditSumma.foreignCurrency ? baseSum.multiply(service.getParamService().getParamValueGlobalNumber("LIMITATION_SALE_PERCENT").divide(100)) : null', 'BASE_SUM_LSP');
 
 update SP.OPER_LIM_OPERATION_GROUP
 set
@@ -14,13 +14,13 @@ and operation_group_code = '-26';
 
 update SP.OPER_LIM_OPERATION_GROUP
 set
-SUB_DELTA_EXPR_ID = '-66'
+SUB_DELTA_EXPR_ID = '-12'
 where OPER_LIM_ID = '-2'
 and OPERATION_GROUP_CODE = '-26';
 
 update SP.OPER_LIM_OPERATION_GROUP
 set
-SUB_DELTA_EXPR_ID = '-55'
+SUB_DELTA_EXPR_ID = '-11'
 where OPER_LIM_ID = '-1'
 and OPERATION_GROUP_CODE = '-26';
 
